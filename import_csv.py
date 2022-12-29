ff1fdf47636fbd1ac891481ca8f46e4192052185
Booka77 <basaa@me.com>
2022-12-29 20:48:39 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	import_csv.py
    #	table_creation.py

--- import_csv.py
+++ import_csv.py
@@ -28,7 +28,7 @@ sql = '''COPY msp_transit
     DELIMITER ','
     CSV HEADER'''
 cursor.execute(sql)
->>>>>>>>> Temporary merge branch 2
+
 
 # Заливка данных в основную таблицу
 # 1. Обновление данных по записям, которые отсутствуют в исходнике - исключены из реестра МСП
@@ -38,8 +38,8 @@ try:
 except:
       pass
 cursor.execute("CREATE TABLE tab1 ( pk varchar(15) )")
-sql = "INSERT INTO tab1 (PK) " \
-      "SELECT msp.ogrn_num AS PK FROM msp WHERE msp.ogrn_num NOT IN (SELECT ogrn_num FROM msp_transit);"
+sql = "INSERT INTO tab1 (pk) " \
+      "SELECT msp.ogrn_num AS pk FROM msp WHERE msp.ogrn_num NOT IN (SELECT ogrn_num FROM msp_transit);"
 cursor.execute(sql)
 # 1.2. меняем даты окончания действия записи по отваливающимся предприятиям (отчетная минус 1)
 sql = "UPDATE msp SET effective_to_dt = " + wDate_minus1Str + " " \
@@ -49,18 +49,18 @@ sql = "UPDATE msp SET effective_to_dt = " + wDate_minus1Str + " " \
 # cursor.execute(sql)
 # cursor.execute("DROP TABLE tab1")
 # 2. Обработка клиентов, по которым меняются реквизиты
-# 2.1. ОБновление дат окончания действия по записям, которые будут актуализироваться
+# 2.1. Отбор кодов записей, которые будут актуализироваться (ключ - ОГРН)
 try:
       cursor.execute("DROP TABLE tab2")
 except:
       pass
 cursor.execute("CREATE TABLE tab2 ( pk varchar(15) )")
-sql = '''INSERT INTO tab1 (pk) 
+sql = '''INSERT INTO tab2 (pk) 
             SELECT tr.pk
             FROM (
                   SELECT msp_adding_dt, msp_type_flg, msp_category_flg, new_msp_flg, social_company_flg, employees_cnt, inn_num, ogrn_num, client_nm 
                   FROM msp 
-                  WHERE is_active_flg = 1 
+                  WHERE effective_to_dt = '5999-12-31' 
                   UNION
                   SELECT msp_adding_dt, msp_type_flg, msp_category_flg, new_msp_flg, social_company_flg, employees_cnt, inn_num, ogrn_num, client_nm 
                   FROM msp_transit) AS tr
@@ -69,5 +69,28 @@ sql = '''INSERT INTO tab1 (pk)
       '''
 cursor.execute(sql)
 
-# 2.2.
+# 2.2. ОБновление дат окончания действия и флагов активности по записям, которые будут актуализироваться
+sql = '''UPDATE msp SET effective_to_dt = ''' + wDate_minus1Str + ''' AND is_active_flg = 0 
+      FROM tab2
+      WHERE (msp.ogrn_num = tab2.pk) AND msp.is_active_flg = 1 and msp.effective_to_dt = '5999-12-31';"
+      '''
+cursor.execute(sql)
+# 2.3. Добавлкние актуализированных записей
+sql = '''INSERT INTO msp (msp_adding_dt, msp_type_flg, msp_category_flg, new_msp_flg, social_company_flg, employees_cnt, inn_num, ogrn_num, client_nm, effective_from_dt, effective_to_dt, is_active_flg)
+            SELECT msp_adding_dt, msp_type_flg, msp_category_flg, new_msp_flg, social_company_flg, employees_cnt, inn_num, ogrn_num, client_nm, ''' + wDateStr + ''' AS effective_from_dt, 
+                  '5999-12-31' AS effective_to_dt, 1 AS is_active_flg
+            FROM msp_target
+            WHERE (msp.ogrn_num = tab2.pk) AND 1=1
+      '''
+cursor.execute(sql)
 # 3. Добавление новых клиентов
+# 3.1. Отбор кодов ОГРН по клиентам, которые будут заливаться в базу впервые
+try:
+      cursor.execute("DROP TABLE tab2")
+except:
+      pass
+cursor.execute("CREATE TABLE tab2 ( pk varchar(15) )")
+sql = "INSERT INTO tab1 (pk) " \
+      "SELECT msp_transit.ogrn_num AS pk FROM msp_transit WHERE msp_transit.ogrn_num NOT IN (SELECT ogrn_num FROM msp WHERE effective_to_dt = '5999-12-31' AND is_active_fkg = 1);"
+cursor.execute(sql)
+# 3.2. Заливка новых записей
