7cac5cf420f967d90e571f70081429d1aeb13004
Booka77 <basaa@me.com>
2022-12-30 16:52:10 +0700

    it's work 02

--- import_csv.py
+++ import_csv.py
@@ -32,12 +32,13 @@ cursor = conn.cursor()
 # очистка транзитной таблицы
 cursor.execute("TRUNCATE TABLE msp_transit")
 # импорт файла csv в транзитную таблицу базы
+print('Идет импорт файла csv в транзитную таблицу базы')
 sql = '''COPY msp_transit 
     FROM '/tmp/msp.csv'
     DELIMITER ','
     CSV HEADER'''
 cursor.execute(sql)
-print('Закачали в транзит')
+
 
 # Заливка данных в основную таблицу
 # 1. Обновление данных по записям, которые отсутствуют в исходнике - исключены из реестра МСП
@@ -46,20 +47,27 @@ try:
       cursor.execute("DROP TABLE tab1")
 except:
       pass
+print('1.1. Создание таблицы с индексами отсутствующих предприятий')
 cursor.execute("CREATE TABLE tab1 ( pk varchar(15) )")
-sql = "INSERT INTO tab1 (pk) " \
-      "SELECT msp.ogrn_num AS pk FROM msp WHERE msp.ogrn_num NOT IN (SELECT ogrn_num FROM msp_transit);"
+# sql = "INSERT INTO tab1 (pk) " \
+#       "SELECT msp.ogrn_num AS pk FROM msp WHERE msp.ogrn_num NOT IN (SELECT ogrn_num FROM msp_transit);"
+sql = '''INSERT INTO tab1 (pk) 
+            SELECT msp.ogrn_num 
+            FROM msp LEFT JOIN msp_transit ON msp_transit.ogrn_num = msp.ogrn_num
+            WHERE msp_transit.ogrn_num IS NULL AND msp.effective_to_dt = '5999-12-31';
+      '''
 cursor.execute(sql)
 outer_count = recCount(cursor, 'tab1')
-# 1.2. меняем даты окончания действия записи по отваливающимся предприятиям (отчетная минус 1)
-sql = "UPDATE msp SET effective_to_dt = " + wDate_minus1Str + " " \
-      "FROM tab1  " \
-      " WHERE (msp.ogrn_num = tab1.pk) AND msp.is_active_flg = 1 and msp.effective_to_dt = '5999-12-31';"
+print('Отсутствующие:', outer_count)
+# 1.2. Меняем даты окончания действия записи по отваливающимся предприятиям (отчетная минус 1)
+print('1.2. Меняем даты окончания действия записи по отваливающимся предприятиям (отчетная минус 1)')
+sql = "UPDATE msp SET effective_to_dt = {0} FROM tab1   WHERE (msp.ogrn_num = tab1.pk) AND msp.is_active_flg = 1 and msp.effective_to_dt = \'5999-12-31\';".format(wDate_minus1Str)
 cursor.execute(sql)
 # cursor.execute("DROP TABLE tab1")
 print('Блок 1')
 # 2. Обработка клиентов, по которым меняются реквизиты
 # 2.1. Отбор кодов записей, которые будут актуализироваться (ключ - ОГРН)
+print('2.1. Отбор кодов записей, которые будут актуализироваться (ключ - ОГРН)')
 try:
       cursor.execute("DROP TABLE tab2")
 except:
@@ -80,6 +88,7 @@ sql = '''INSERT INTO tab2 (pk)
 cursor.execute(sql)
 
 # 2.2. ОБновление дат окончания действия и флагов активности по записям, которые будут актуализироваться
+print('2.2. ОБновление дат окончания действия и флагов активности по записям, которые будут актуализироваться')
 sql = '''UPDATE msp SET effective_to_dt = ''' + wDate_minus1Str + ''', is_active_flg = 0 
       FROM tab2
       WHERE (msp.ogrn_num = tab2.pk) AND msp.is_active_flg = 1 and msp.effective_to_dt = '5999-12-31';
@@ -95,9 +104,11 @@ sql = '''INSERT INTO msp (msp_adding_dt, msp_type_flg, msp_category_flg, new_msp
       '''
 cursor.execute(sql)
 change_count = recCount(cursor,'msp') - change_count0
+print('Обновленные:', change_count)
 print('Блок 2')
 # 3. Добавление новых клиентов
 # 3.1. Отбор кодов ОГРН по клиентам, которые будут заливаться в базу впервые
+print('3.1. Отбор кодов ОГРН по клиентам, которые будут заливаться в базу впервые')
 try:
       cursor.execute("DROP TABLE tab2")
 except:
@@ -112,6 +123,7 @@ sql = '''INSERT INTO tab2 (pk)
       '''
 cursor.execute(sql)
 # 3.2. Заливка новых записей
+print('3.2. Заливка новых записей')
 add_count0 = recCount(cursor, 'msp')
 sql = '''INSERT INTO msp (msp_adding_dt, msp_type_flg, msp_category_flg, new_msp_flg, social_company_flg, employees_cnt, inn_num, ogrn_num, client_nm, effective_from_dt, effective_to_dt, is_active_flg)
             SELECT msp_adding_dt, msp_type_flg, msp_category_flg, new_msp_flg, social_company_flg, employees_cnt, inn_num, ogrn_num, client_nm, msp_adding_dt AS effective_from_dt, 
